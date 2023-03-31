b2f4f960d38fc07fe1a49f25a4e198eef816ba99
Yuriy Mironenko <ydtalel@gmail.com>
2023-03-31 21:15:28 +0500

    hw finished

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -15,7 +15,7 @@ class Teacher(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    teachers = models.ManyToManyField(Teacher, related_name='students')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
@@ -24,3 +24,9 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+    def __str__(self):
+        return "%s (%s,%s)" % (
+            self.name, ", ".join(teacher.name for teacher in self.teachers.all()),
+            ", ".join(teacher.subject for teacher in self.teachers.all())
+        )
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school',
+
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,6 +79,8 @@ DATABASES = {
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'PASSWORD',
     }
 }
 
@@ -131,3 +136,6 @@ try:
 except ImportError:
     pass
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
