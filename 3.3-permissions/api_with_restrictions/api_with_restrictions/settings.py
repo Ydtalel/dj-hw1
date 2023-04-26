e9ce4156687f5860b1b4d4783badfc7d5c24fa20
Yuriy Mironenko <ydtalel@gmail.com>
2023-04-26 10:08:06 +0500

    views.py chanched

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -91,8 +91,7 @@ DATABASES = {
         'NAME': 'netology_classified_ads',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': 'yu14r06iy90',
+
     }
 }
 
